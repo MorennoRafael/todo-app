589e56d042b09e8350086f4014f56cce00a0b2c6
MorennoRafael <morennorafael31@gmail.com>
2026-01-23 16:32:45 +0700

    new step 5

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,34 +1,3 @@
-// import MainLayout from "../layouts/MainLayout";
-// import BackButton from "../components/form/BackButton";
-// import ButtonForm from "../components/form/ButtonForm";
-// import { FaCirclePlus } from "react-icons/fa6";
-
-
-// export default function AddTodo() {
-//     return (
-//         <MainLayout>
-//             <div className="bg-white p-6 rounded-2xl shadow-md shadow-neutral-400 flex flex-col gap-4">
-//                 <h2 className="text-xl font-semibold text-[#4285F4]">Add New Todo</h2>
-
-//                 <div className="flex flex-col gap-1">
-//                     <label className="text-sm font-medium">Isi Todo</label>
-//                     <textarea
-//                         className="border border-neutral-300 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-[#4285F4]"
-//                         rows={4}
-//                         placeholder="Tulis aktivitas yang ingin kamu kerjakan..."
-//                     ></textarea>
-//                 </div>
-
-//                 <div className="flex justify-between mt-2">
-//                     <BackButton href="/"/>
-
-//                     <ButtonForm color="#34A853" icon={<FaCirclePlus size={18} />} text="Tambah" href="/"/>
-//                 </div>
-//             </div>
-//         </MainLayout>
-//     )
-// }
-
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
